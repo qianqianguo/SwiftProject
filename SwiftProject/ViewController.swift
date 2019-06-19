5412e82414cdea2e5fc89132ce94047f3a6501e4
Jivan <1330336293@qq.com>
2019-06-19 14:23:49 +0800

    first commit

---
+++ SwiftProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftProject
+//
+//  Created by qmai on 2019/6/19.
+//  Copyright © 2019年 Anhui YiYun Technologies Co. , Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
