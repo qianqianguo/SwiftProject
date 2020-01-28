ee3da143a2cbeeebee62bb6768e9db2f2f0efb97
Jivan-ios <1330336293@qq.com>
2020-01-28 21:50:31 +0800

    delete

--- Pods/Target Support Files/Pods-SwiftProjectTests/Pods-SwiftProjectTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SwiftProjectTests : NSObject
-@end
-@implementation PodsDummy_Pods_SwiftProjectTests
-@end
--- README.md
+++
@@ -1,2 +0,0 @@
-# SwiftProject
-Swift项目框架
